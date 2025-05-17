af374a39b7ce56abbfb57f56f2d42e83f93ab628
seomoon <seomoon@seomoonui-MacBookPro.local>
2025-05-17 15:35:49 +0900

    init: 프로젝트 설정

---
+++ src/main/java/com/practice/pickcarprac/PickcarpracApplication.java
@@ -0,0 +1,13 @@
+package com.practice.pickcarprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickcarpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickcarpracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/pickcarprac/PickcarpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pickcarprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickcarpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
