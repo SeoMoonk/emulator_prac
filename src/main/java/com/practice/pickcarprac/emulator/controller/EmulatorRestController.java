8419122c76e0624a87a845ab13fe913a0fd3c580
SeoMoonk <sunghoon1216@naver.com>
2025-05-17 19:40:48 +0900

    feat: 프론트에서 id와 on/off요청을 보냄

--- src/main/java/com/practice/pickcarprac/emulator/controller/EmulatorRestController.java
+++ src/main/java/com/practice/pickcarprac/emulator/controller/EmulatorRestController.java
@@ -2,6 +2,8 @@ package com.practice.pickcarprac.emulator.controller;
 
 import com.practice.pickcarprac.emulator.service.EmulatorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/emulator")
 @RequiredArgsConstructor
+@Slf4j
 public class EmulatorRestController {
 
     private final EmulatorService emulatorService;
 
-    @PostMapping("/on")
-    public void on() {
+    @PostMapping("/on/{emulatorId}")
+    public void on(@PathVariable(value = "emulatorId") Long emulatorId) {
         //Todo: step1. 시동이 켜짐
+        log.debug("On => input emulatorId: {}", emulatorId);
     }
 
     @PostMapping("/receive")
@@ -23,8 +27,9 @@ public class EmulatorRestController {
         //Todo: step2. 주기 데이터를 전달받음
     }
 
-    @PostMapping("/off")
-    public void off() {
+    @PostMapping("/off/{emulatorId}")
+    public void off(@PathVariable(value = "emulatorId") Long emulatorId) {
         //Todo: step3. 시동이 꺼짐
+        log.debug("Off => input emulatorId: {}", emulatorId);
     }
 }
